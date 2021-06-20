642482cd45ffa1773f926faf05a656fa3de67909
brennanashley <ashley-brennan@lambdastudents.com>
2021-06-20 18:38:07 -0500

    spotify updates

--- app/db.py
+++ app/db.py
@@ -1,6 +1,8 @@
 """Database functions"""
 
 import os
+import sqlite3
+import pandas as pd
 
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends
@@ -17,9 +19,15 @@ async def get_db() -> sqlalchemy.engine.base.Connection:
 
     Otherwise uses a SQLite database for initial local development.
     """
-    load_dotenv()
-    database_url = os.getenv('DATABASE_URL', default='sqlite:///temporary.db')
-    engine = sqlalchemy.create_engine(database_url)
+    #load_dotenv()
+    #database_url = os.getenv('DATABASE_URL', default='sqlite:///temporary.db')
+    #engine = sqlalchemy.create_engine(database_url)
+    engine = sqlalchemy.create_engine('sqlite:///spotify_songs_db.sqlite3')
+    #sq_con = sqlite3.connect('spotify_songs_db.sqlite3')
+    #sq_curs = sq_con.cursor()
+    # load the data into a pandas dataframe and set the index
+    song_data = pd.read_csv('data_o_clean.csv')
+    song_data.to_sql('data_o_clean', con=engine, if_exists='replace')
     connection = engine.connect()
     try:
         yield connection
@@ -38,3 +46,4 @@ async def get_url(connection=Depends(get_db)):
     """
     url_without_password = repr(connection.engine.url)
     return {'database_url': url_without_password}
+
